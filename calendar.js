458a1b878b309565b944fdbed64c3bb0bc6a8b36
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-10-30 14:36:42 -0500

    Update calendar.js
    added js to create a calendar for months w/ 30 days and february w/ 28 days and 29 days on leap years
    still need to account for shifting the calendar to start 1 at a different box (ie 11/1/23 should start on the 4th box)

--- calendar.js
+++ calendar.js
@@ -29,6 +29,7 @@ const dateBoxContainer=document.getElementById("date_boxes")
 for (let i=0; i<=months.length; i++){
 	console.log("in the i for loop"+i)
   console.log(currentDate.getMonth())
+  //if(6==i){  //For Debugging testing
 	if(currentDate.getMonth()==i){
   	document.getElementById("Month_year").textContent=`${months[i]} ${currentDate.getFullYear()}`;
   	console.log(months[i])
@@ -44,5 +45,36 @@ for (let i=0; i<=months.length; i++){
     
     	}
     }
+    if(i==3||i==5||i==8||i==10){
+    	for (let j=0; j<=34; j++){
+      	console.log("in the j for loop")
+    		let dateBox =document.createElement("div");
+   	 		dateBox.className="Day";
+        dateBoxContainer.appendChild(dateBox)
+        if(j<30){
+        	dateBox.textContent=j+1;
+        }
   }
-}  
+}
+		if(i==1){
+    	for (let j=0; j<=34; j++){
+      	console.log("in the j for loop")
+    		let dateBox =document.createElement("div");
+   	 		dateBox.className="Day";
+        dateBoxContainer.appendChild(dateBox);
+        //if(4%4==0){
+        if(currentDate.getFullYear()%4==0){
+        	if(j<29){
+        	dateBox.textContent=j+1;
+        	}
+        }
+        else {
+          if(j<28){
+        		dateBox.textContent=j+1;
+        		}
+        }
+      }
+      
+    }
+  } 
+}
